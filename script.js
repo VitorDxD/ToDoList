6d290380f7da09eab1a65c1f7f867e5b3ae6866c
VitorDxD <vitornunes2747@gmail.com>
2022-05-18 15:49:33 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const elementoTexto = document.getElementById("Texto")
+const elementoAdd = document.getElementById("Add")
+const elementoDelete = document.getElementById("Delete")
+const elementoLista = document.querySelector("ul")
+
+    if (localStorage.getItem("arraySalva") === ""){
+        var tarefas = []
+    } else{
+        var tarefas = JSON.parse(localStorage.getItem("arraySalva"))
+    }
+
+function mostrarTarefa(){
+    elementoLista.innerHTML = ''
+
+    for (tarefa of tarefas){
+        const elementoTarefa = document.createElement('li')
+        const textoTarefa = document.createTextNode(tarefa)
+
+        const elementoLink = document.createElement('a')
+        const pos = tarefas.indexOf(tarefa)
+
+        const linkText = document.createTextNode('X') 
+        elementoLink.appendChild(linkText)
+
+        elementoLink.setAttribute('onclick',`deletarTarefa(${pos})`)
+
+        elementoTarefa.appendChild(textoTarefa)
+        elementoLista.appendChild(elementoTarefa)
+        elementoTarefa.appendChild(elementoLink)
+    }
+}
+
+
+
+function addTarefa(){
+    if (elementoTexto.value != ''){
+
+        const textoTarefa = elementoTexto.value
+        tarefas.push(textoTarefa)
+        elementoTexto.value = ''
+
+        mostrarTarefa()
+        localStorage.setItem("arraySalva", JSON.stringify(tarefas))
+    } else {
+        window.alert('O campo está vázio!')
+    }
+    
+}
+
+elementoAdd.setAttribute('onclick','addTarefa()')
+
+
+function deletarTarefa(pos){
+    tarefas.splice(pos, 1)
+    mostrarTarefa()
+    localStorage.setItem("arraySalva", JSON.stringify(tarefas))
+}
+
+function limpar(){
+    localStorage.setItem("arraySalva", "")
+    tarefas = ""
+    mostrarTarefa()
+}
